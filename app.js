8e37476ee2514b89f44e81e738024eb97f0b222a
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-10-03 18:57:00 -0400

    feat: enable ejs

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const ejs = require("ejs");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middleware
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Connected");
+  res.render("home");
 });
 
 app.listen(PORT, () => {
